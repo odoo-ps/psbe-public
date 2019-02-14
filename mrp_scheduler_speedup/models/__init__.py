2abc03e0de6c2169b9cfaef4f64aaa49c78ffc34
Bart Van Bossche <bvb@odoo.com>
2019-02-14 11:51:52 +0100

    [ADD] v12.0 branch

--- mrp_scheduler_speedup/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import orderpoint
-from . import quant
-from . import stock_rule
-from . import stock_move
--- mrp_scheduler_speedup/models/orderpoint.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class Orderpoint(models.Model):
-    """ Defines Minimum stock rules. """
-    _inherit = "stock.warehouse.orderpoint"
-
-    process_date = fields.Datetime()
-    force_reprocess = fields.Boolean(default=False)

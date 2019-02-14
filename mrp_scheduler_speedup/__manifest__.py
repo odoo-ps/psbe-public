8955d5e33b18ccf788c3368640558c730f73b0b5
Bart Van Bossche <bvb@odoo.com>
2019-02-14 11:59:26 +0100

    [ADD] add speedup for MRP scheduler cron

---
+++ mrp_scheduler_speedup/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'scheduler_fix',
+    'description': """Fix for scheduler with reordering rules""",
+    'depends': [
+        'stock'
+    ],
+    'data': [
+
+    ],
+}
